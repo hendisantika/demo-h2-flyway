888bd64efcc83582d7c86297391efd7a861cd7ad
Hendi Santika <hendisantika@gmail.com>
2021-10-15 06:56:13 +0700

    #4 - Add init table data migration script

---
+++ src/main/resources/db/migration/V2.0.0_20211013_1050__Init_Account_Data.sql
@@ -0,0 +1,6 @@
+INSERT INTO account(id, login_id, login_name, status, type)
+VALUES (1, 1, 'Uzumaki Naruto', 'ACTIVATE', 'BRN'),
+       (2, 2, 'Uzumaki Naruto', 'INACTIVATE', 'NRIC'),
+       (3, 3, 'Uchiha Sasuke', 'LOCK', 'NRIC'),
+       (4, 4, 'Hatake Kakashi', 'ARCHIVE', 'NRIC'),
+       (5, 5, 'Uchiha Madara', 'ARCHIVE', 'NRIC');
