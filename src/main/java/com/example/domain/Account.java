378b04c3f69132bf6b138e8e43e27e2283217f03
hendisantika <hendisantika@yahoo.co.id>
2023-08-04 20:50:24 +0700

    #64 - Upgraded into Spring Boot 3.1.1 version ✏️📗:octocat:⬆ Hendi Santika* 2 minutes ago

--- src/main/java/com/example/domain/Account.java
+++ src/main/java/com/example/domain/Account.java
@@ -2,18 +2,10 @@ package com.example.domain;
 
 import com.example.domain.enumerate.AccountStatus;
 import com.example.domain.enumerate.AccountType;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 /**
  * Created by IntelliJ IDEA.
